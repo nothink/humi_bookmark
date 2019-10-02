8f9bf33f84138ef066f3f5f6c2ada724fac051c4
nothink <gakongakon@gmail.com>
2019-10-02 12:18:04 +0900

    Remove for wasabi aws.

--- humi_bookmark/js/upload.js
+++ humi_bookmark/js/upload.js
@@ -1,9 +1,3 @@
-AWS.config.update({
-    accessKeyId: wasabi.accessKeyId,
-    secretAccessKey: wasabi.secretAccessKey,
-    region: wasabi.region
-});
-
 // remove array's duplicates
 function removeArrayDuplicates(array) {
     return array.filter(function(value, index, self) {
@@ -19,58 +13,20 @@ function removeDoubleSlash(string) {
 // upload URL arrays to wasabi storage (type: text/plain)
 function upload(array) {
     if (array.length > 0) {
-        let sts = new AWS.STS( {endpoint: wasabi.stsEndpoint });
-        sts.assumeRole({
-            RoleArn: wasabi.stsRoleArn,
-            RoleSessionName: 'humi_bookmark',
-            DurationSeconds: 1200
-        }, (err, data) => {
-            if (err) {
-                console.error('Cannot assume role');
-                console.error(err, err.stack);
-            } else {
-                // remove dups
-                let uploads = removeArrayDuplicates(array).sort();
-                uploads = uploads.map(removeDoubleSlash);
+        // remove dups
+        let uploads = removeArrayDuplicates(array).sort();
+        uploads = uploads.map(removeDoubleSlash);
 
-                // set access keys and region
-                AWS.config.update({
-                    accessKeyId: data.Credentials.AccessKeyId,
-                    secretAccessKey: data.Credentials.SecretAccessKey,
-                    sessionToken: data.Credentials.SessionToken,
-                    region: wasabi.region
-                });
-
-                let s3 = new AWS.S3({
-                    endpoint: new AWS.Endpoint(wasabi.endpoint),
-                    params: { Bucket: 'humi-bookmark' }
-                });
-
-                s3.putObject({
-                    Key: Date.now().toString() + '.log',
-                    ContentType: 'text/plain',
-                    Body: uploads.join("\n") + '\n'
-                }, (err, data) => {
-                    if (err) {
-                        console.error('Failed to put objects');
-                        console.error(err, err.stack);
-                        return [];
-                    } else {
-                        return uploads;
-                    }
-                });
-
-                fetch('https://yurararan.nothink.jp/api/v0/resources', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        'urls': uploads,
-                    }),
-                }).catch(console.error);
-            }
-        });
+        // put array to api.
+        fetch('https://yurararan.nothink.jp/api/v0/resources', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'urls': uploads,
+            }),
+        }).catch(console.error);
     }
 }
--- humi_bookmark/js/wasabi.js
+++
@@ -1,8 +0,0 @@
-const wasabi = {
-    accessKeyId: '7RHAZOIGLCETV4Y83HMF',
-    secretAccessKey: 'B6oPITfLzrmKFGiNZyyCa2eo5L2V2ee1JlSsZFEF',
-    region: 'us-east-1',
-    endpoint: 's3.wasabisys.com',
-    stsEndpoint: 'https://sts.wasabisys.com',
-    stsRoleArn: 'arn:aws:iam::100000010195:role/StsAllowFromHumiBookmark'
-};
