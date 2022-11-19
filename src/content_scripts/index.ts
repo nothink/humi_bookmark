f81f6677cb3703da071cdd6fff4a0dc84380fa59
nothink <gakongakon@gmail.com>
2022-11-19 10:45:26 +0900

    fix: create stubs for content_scripts

---
+++ src/content_scripts/index.ts
@@ -0,0 +1,15 @@
+export { }
+
+const entry = () => {
+  const div = document.createElement('div')
+  div.id = 'hoge'
+  div.style.position = 'fixed'
+  div.style.top = '64px'
+  div.style.left = '1px'
+  div.style.width = '7px'
+  div.style.height = '32px'
+  div.style.backgroundColor = 'red'
+  document.body.appendChild(div)
+}
+
+entry()
