a28a3c471edde5494da1b3257a0bd2bc166941ea
nothink <gakongakon@gmail.com>
2022-12-04 01:10:24 +0900

    fix: more simplify

--- src/content_scripts/retrieve.ts
+++ src/content_scripts/retrieve.ts
@@ -2,29 +2,10 @@ import { parseDoc } from '../lib/parse'
 import { upload } from '../lib/upload'
 
 export const retrieveVcards = (): void => {
-  const regex =
-    /((c.|)stat100.ameba.jp)|([a-z0-9]*\.cloudfront\.net)\/vcard\/[-a-zA-Z0-9/._+]*\.(?!build)[a-zA-Z0-9]+/gm
   const parsed = parseDoc(document)
-  parsed.forEach((p) => {
-    if (p.length > 0) {
-      // replace all '\/' to '/'
-      p = p.replace(/\\\//g, '/')
-      const files: string[] = []
-      let m
-      while ((m = regex.exec(p)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-          regex.lastIndex++
-        }
-
-        // The result can be accessed through the `m`-variable.
-        // replace all '//' to '/'
-        const key = m[0].replace(/\/\//g, '/')
-        files.push(key)
-      }
-      if (files.length > 0) {
-        upload(files)
-      }
-    }
-  })
+  const regex = /vcard\/[-_.a-zA-Z0-9/+]*\.[a-zA-Z0-9]+/g
+  const matches = Array.from(parsed.matchAll(regex), (m) => m[0])
+  if (matches.length > 0) {
+    upload(matches)
+  }
 }
--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,33 +1,21 @@
-// remove array's duplicates
-function removeArrayDuplicates(array: string[]): string[] {
-  return array.filter(function (value, index, self) {
-    return self.indexOf(value) === index
-  })
-}
-
-// remove double slash path to single slash
-function removeDoubleSlash(string: string): string {
-  return 'https://' + string.replace('//', '/')
-  // return string.replace('//', '/');
-}
-
-export const upload = (array: string[]): void => {
-  if (array.length > 0) {
+export const upload = (vcards: string[]): void => {
+  if (vcards.length > 0) {
     // remove dups
-    let uploads = removeArrayDuplicates(array).sort()
-    uploads = uploads.map(removeDoubleSlash)
+    const nodups = [...new Set(vcards)]
+    const urls = nodups.map(
+      (vcard) => `https://dqx9mbrpz1jhx.cloudfront.net/${vcard}`
+    )
 
     // put array to api.
-    const endpoint2 =
+    const endpoint =
       'https://asia-northeast1-seioclub.cloudfunctions.net/dqx9mbrpz1jhx'
-    const uploads2 = { urls: uploads }
-    fetch(endpoint2, {
+    fetch(endpoint, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(uploads2),
+      body: JSON.stringify({ urls }),
     }).catch(console.error)
   }
 }
