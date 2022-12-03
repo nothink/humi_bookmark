ab14df5dbaa13b0b322ad7e49372d232b7180cc2
nothink <gakongakon@gmail.com>
2022-12-03 22:59:48 +0900

    fix: update retrieve funcs

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -6,61 +6,27 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/nextSibling
 
 /**
+ * Stringfy Node element
+ * @param node HTML Node
+ * @returns a string
+ */
+const stringifyNode = (node: ChildNode): string => {
+  if (node instanceof Element) {
+    return node.outerHTML
+  }
+  return node.nodeValue ?? ''
+}
+
+/**
  * Parse Document to array of string
  * @param doc HTML Document
  * @returns array of strings
  */
 export const parseDoc = (doc: Document): string[] => {
   const result: string[] = []
-  let node: Node | null = doc.firstChild
-
-  while (node != null) {
-    switch (node.nodeType) {
-      case Node.ELEMENT_NODE:
-        if (node instanceof Element) {
-          const html = node.outerHTML
-          console.log(html)
-          result.push(html)
-        }
-        break
-      case Node.TEXT_NODE:
-        if (node.nodeValue !== null) {
-          const text = node.nodeValue
-          console.log(text)
-          result.push(text)
-        }
-        break
-      case Node.CDATA_SECTION_NODE:
-        if (node.nodeValue !== null) {
-          const cdata = '<![CDATA[' + node.nodeValue + ']]>'
-          console.log(cdata)
-          result.push(cdata)
-        }
-        break
-      case Node.COMMENT_NODE:
-        if (node.nodeValue !== null) {
-          const comment = '<!--' + node.nodeValue + '-->'
-          console.log(comment)
-          result.push(comment)
-        }
-        break
-      case Node.DOCUMENT_TYPE_NODE:
-        if (node instanceof DocumentType) {
-          // (X)HTML documents are identified by public identifiers
-          const doctype =
-            '<!DOCTYPE ' +
-            node.name +
-            (node.publicId !== '' ? ' PUBLIC "' + node.publicId + '"' : '') +
-            (node.publicId === '' && node.systemId !== '' ? ' SYSTEM' : '') +
-            (node.systemId !== '' ? ' "' + node.systemId + '"' : '') +
-            '>\n'
-          console.log(doctype)
-          result.push(doctype)
-        }
-        break
-    }
-    console.log('next...')
-    node = node.nextSibling
-  }
+  const nodes = doc.childNodes
+  nodes.forEach((node: ChildNode): void => {
+    result.push(stringifyNode(node))
+  })
   return result
 }
