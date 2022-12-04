742ef6a4c6189e45642fa87d8e6980dd1f4ccc37
nothink <gakongakon@gmail.com>
2022-12-04 14:59:30 +0900

    fix: clean up and do more compactly

--- src/content_scripts/retrieve.ts
+++ src/content_scripts/retrieve.ts
@@ -5,7 +5,14 @@ export const retrieveVcards = (): void => {
   const parsed = parseDoc(document)
   const regex = /vcard\/[-_.a-zA-Z0-9/+]*\.[a-zA-Z0-9]+/g
   const matches = Array.from(parsed.matchAll(regex), (m) => m[0])
-  if (matches.length > 0) {
-    upload(matches)
+  // remove dups
+  const nodups = [...new Set(matches)]
+  // create urls
+  const urls = nodups.map(
+    (vcard) => `https://dqx9mbrpz1jhx.cloudfront.net/${vcard}`
+  )
+
+  if (urls.length > 0) {
+    upload(urls)
   }
 }
--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,21 +1,13 @@
-export const upload = (vcards: string[]): void => {
-  if (vcards.length > 0) {
-    // remove dups
-    const nodups = [...new Set(vcards)]
-    const urls = nodups.map(
-      (vcard) => `https://dqx9mbrpz1jhx.cloudfront.net/${vcard}`
-    )
-
-    // put array to api.
-    const endpoint =
-      'https://asia-northeast1-seioclub.cloudfunctions.net/dqx9mbrpz1jhx'
-    fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ urls }),
-    }).catch(console.error)
-  }
+export const upload = (urls: string[]): void => {
+  // put array to api.
+  const endpoint =
+    'https://asia-northeast1-seioclub.cloudfunctions.net/dqx9mbrpz1jhx'
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ urls }),
+  }).catch(console.error)
 }
