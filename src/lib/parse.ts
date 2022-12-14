24fc7cda7e4217c822bdbf05e0f2bcce6b04f1e4
nothink (Satoshi Ishii) <gakongakon@gmail.com>
2022-12-14 22:44:11 +0900

    add vitest (#26)
    * fix: more simplify
    
    * fix: clean up and do more compactly
    
    * fix: update asdf tool versions
    
    * fix: create empty file only
    
    * fix: pnpm update
    
    * fix: pnpm update only

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,10 +1,3 @@
-// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
-// Demo: let serialized_html = DOMtoString(document);
-
-// TODO: want to fix more testable.
-// https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
-// https://developer.mozilla.org/en-US/docs/Web/API/Node/nextSibling
-
 /**
  * Stringfy Node element
  * @param node HTML Node
@@ -22,11 +15,11 @@ const stringifyNode = (node: ChildNode): string => {
  * @param doc HTML Document
  * @returns array of strings
  */
-export const parseDoc = (doc: Document): string[] => {
-  const result: string[] = []
+export const parseDoc = (doc: Document): string => {
+  const strs: string[] = []
   const nodes = doc.childNodes
   nodes.forEach((node: ChildNode): void => {
-    result.push(stringifyNode(node))
+    strs.push(stringifyNode(node))
   })
-  return result
+  return strs.join('\n')
 }
--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,33 +1,13 @@
-// remove array's duplicates
-function removeArrayDuplicates(array: string[]): string[] {
-  return array.filter(function (value, index, self) {
-    return self.indexOf(value) === index
-  })
-}
-
-// remove double slash path to single slash
-function removeDoubleSlash(string: string): string {
-  return 'https://' + string.replace('//', '/')
-  // return string.replace('//', '/');
-}
-
-export const upload = (array: string[]): void => {
-  if (array.length > 0) {
-    // remove dups
-    let uploads = removeArrayDuplicates(array).sort()
-    uploads = uploads.map(removeDoubleSlash)
-
-    // put array to api.
-    const endpoint2 =
-      'https://asia-northeast1-seioclub.cloudfunctions.net/dqx9mbrpz1jhx'
-    const uploads2 = { urls: uploads }
-    fetch(endpoint2, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(uploads2),
-    }).catch(console.error)
-  }
+export const upload = (urls: string[]): void => {
+  // put array to api.
+  const endpoint =
+    'https://asia-northeast1-seioclub.cloudfunctions.net/dqx9mbrpz1jhx'
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ urls }),
+  }).catch(console.error)
 }
